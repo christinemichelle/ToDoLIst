b5453288db9452fb25adb0b9ff5be7f233d9302a
Christinemichelle <christinekito39@gmail.com>
2023-08-22 17:05:35 +0300

    Initial commit

---
+++ app/src/main/java/com/example/todolist/LogInActivity.kt
@@ -0,0 +1,20 @@
+package com.example.todolist
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class LogInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        var sup: TextView = findViewById(R.id.sup)
+        sup.setOnClickListener {
+            var nxt_intent = Intent(this,SignUpActivity::class.java)
+            startActivity(nxt_intent)
+        }
+    }
+}
