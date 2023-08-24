7364908c7de6037ed2e6acfca1cd670f389c1b65
Christinemichelle <christinekito39@gmail.com>
2023-08-24 13:59:29 +0300

    My app

--- app/src/main/java/com/example/todolist/SignUpActivity.kt
+++ app/src/main/java/com/example/todolist/SignUpActivity.kt
@@ -1,11 +1,21 @@
 package com.example.todolist
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class SignUpActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        var back:TextView = findViewById(R.id.txtlogin)
+        back.setOnClickListener {
+            var goback = Intent (this,LogInActivity::class.java )
+            startActivity(goback)
+        }
     }
 }
--- app/src/main/java/com/example/todolist/WelcomeActivity.kt
+++ app/src/main/java/com/example/todolist/WelcomeActivity.kt
@@ -1,11 +1,19 @@
 package com.example.todolist
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+
+        var task: Button = findViewById(R.id.btnnxt)
+        task.setOnClickListener {
+            var gototask= Intent(this, TaskActivity::class.java)
+            startActivity(gototask)
+        }
     }
 }
