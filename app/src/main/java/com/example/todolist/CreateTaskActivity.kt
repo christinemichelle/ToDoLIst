84115d1c7a563bec7623ea9c7ca4a5df73961309
Christinemichelle <christinekito39@gmail.com>
2023-08-29 22:58:47 +0300

    My app

--- app/src/main/java/com/example/todolist/CreateTaskActivity.kt
+++ app/src/main/java/com/example/todolist/CreateTaskActivity.kt
@@ -3,8 +3,10 @@ package com.example.todolist
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 
 class CreateTaskActivity : AppCompatActivity() {
 
@@ -14,5 +16,20 @@ class CreateTaskActivity : AppCompatActivity() {
         setContentView(R.layout.activity_create_task)
 
 
+        val buttonShowToast: Button = findViewById(R.id.btncreate)
+
+        buttonShowToast.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(view: View) {
+                // Display a toast message when the button is clicked
+                Toast.makeText(applicationContext, "Button Clicked!", Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 }
+
+
+
+
+
+
+
--- app/src/main/java/com/example/todolist/WelcomeActivity.kt
+++ app/src/main/java/com/example/todolist/WelcomeActivity.kt
@@ -3,6 +3,7 @@ package com.example.todolist
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 
 class WelcomeActivity : AppCompatActivity() {
@@ -10,10 +11,13 @@ class WelcomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
 
-        var task: Button = findViewById(R.id.btnnxt)
-        task.setOnClickListener {
-            var gototask= Intent(this, TaskActivity::class.java)
-            startActivity(gototask)
+
+        var create: View? = findViewById(R.id.fab)
+        if (create != null) {
+            create.setOnClickListener {
+                val nextintent =Intent(this,CreateTaskActivity::class.java)
+                startActivity(nextintent)
+            }
         }
     }
 }
